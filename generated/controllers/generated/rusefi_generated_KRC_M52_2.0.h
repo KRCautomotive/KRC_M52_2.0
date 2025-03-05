ffbd4b6da0e775b38d6d0066cd75c77b65d09a49
GitHub git update Action <action@github.com>
2025-03-05 01:09:15 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_KRC_M52_2.0.h
+++ generated/controllers/generated/rusefi_generated_KRC_M52_2.0.h
@@ -1387,7 +1387,7 @@
 #define show_tcu_gauges false
 #define show_test_presets true
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 1396324305
+#define SIGNATURE_HASH 3374546791
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1882,7 +1882,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.03.04.KRC_M52_2.0.1396324305"
+#define TS_SIGNATURE "rusEFI main.2025.03.05.KRC_M52_2.0.3374546791"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
--- generated/controllers/generated/signature_KRC_M52_2.0.h
+++ generated/controllers/generated/signature_KRC_M52_2.0.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 1396324305
-#define TS_SIGNATURE "rusEFI main.2025.03.04.KRC_M52_2.0.1396324305"
+#define SIGNATURE_HASH 3374546791
+#define TS_SIGNATURE "rusEFI main.2025.03.05.KRC_M52_2.0.3374546791"
