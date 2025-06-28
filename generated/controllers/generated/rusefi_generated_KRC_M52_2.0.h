857c1f48215f751316a9757f9068c4e2d592651b
GitHub git update Action <action@github.com>
2025-06-28 01:16:59 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_KRC_M52_2.0.h
+++ generated/controllers/generated/rusefi_generated_KRC_M52_2.0.h
@@ -2030,7 +2030,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.06.27.KRC_M52_2.0.268747104"
+#define TS_SIGNATURE "rusEFI main.2025.06.28.KRC_M52_2.0.268747104"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
