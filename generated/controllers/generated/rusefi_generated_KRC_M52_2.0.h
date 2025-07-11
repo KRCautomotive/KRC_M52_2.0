d02976ee20ab8ddfbd351f1cf578688a2d6b7a77
GitHub git update Action <action@github.com>
2025-07-11 01:21:51 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_KRC_M52_2.0.h
+++ generated/controllers/generated/rusefi_generated_KRC_M52_2.0.h
@@ -1497,7 +1497,7 @@
 #define show_tcu_gauges false
 #define show_test_presets true
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 1718688338
+#define SIGNATURE_HASH 879903693
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2037,7 +2037,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.07.10.KRC_M52_2.0.1718688338"
+#define TS_SIGNATURE "rusEFI main.2025.07.11.KRC_M52_2.0.879903693"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- generated/controllers/generated/signature_KRC_M52_2.0.h
+++ generated/controllers/generated/signature_KRC_M52_2.0.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 1718688338
-#define TS_SIGNATURE "rusEFI main.2025.07.10.KRC_M52_2.0.1718688338"
+#define SIGNATURE_HASH 879903693
+#define TS_SIGNATURE "rusEFI main.2025.07.11.KRC_M52_2.0.879903693"
